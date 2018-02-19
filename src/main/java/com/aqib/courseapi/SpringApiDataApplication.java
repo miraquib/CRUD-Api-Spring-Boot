26f504779efa84b06cca93e2737aaf205c1fb1d0
mir_aquib <mir_aquib@yahoo.com>
2018-02-18 19:24:26 -0600

    CRUD api using spring boot

---
+++ src/main/java/com/aqib/courseapi/SpringApiDataApplication.java
@@ -0,0 +1,12 @@
+package com.aqib.courseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiDataApplication.class, args);
+	}
+}
---
+++ src/main/java/com/aqib/courseapi/Topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.aqib.courseapi.Topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
+
